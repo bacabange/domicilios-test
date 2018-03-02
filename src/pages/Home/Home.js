16d98a9b3629adb1d98410805ce329a8b276db8b
Stiven <stivencastillo.90@gmail.com>
2018-03-02 00:11:40 -0500

    Create Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from './Home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className={ styles.app }>
+      </div>
+    );
+  }
+}
+
+export default Home;
